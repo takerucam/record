b76b98455abe6d0178f2d82b9e4a005b9688f970
Takeru Miyake <m18kecam777@gmail.com>
2023-06-18 19:14:02 +0900

    ⏫ update

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p>mainです</p>
+    </main>
+  )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,7 @@
 module.exports = {
   mode: 'jit',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
